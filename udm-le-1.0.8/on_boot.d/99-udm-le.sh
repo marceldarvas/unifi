1964cc86f8f8fadfa11fb820d419470e1f6ec394
Marcel Darvas <hello@marceldarvas.com>
2021-05-23 16:33:40 -0400

    directory files

---
+++ udm-le-1.0.8/on_boot.d/99-udm-le.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Load Environmnt Variables
+. /mnt/data/udm-le/udm-le.env
+
+if [ ! -f /etc/cron.d/udm-le ]; then
+	# Sleep for 5 minutes to avoid restarting
+	# services during system startup.
+	sleep 300
+	sh ${UDM_LE_PATH}/udm-le.sh bootrenew
+fi
