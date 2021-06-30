c5baf6b4edfcbf1e3187710c39e5cf4ba96a383c
Marcel Darvas <marcel@overace.agency>
2021-06-30 19:30:32 -0400

    10-dns.sh cleanup

--- 10-dns.sh
+++ 10-dns.sh
@@ -3,7 +3,19 @@
 ## configuration variables:
 VLAN=5
 IPV4_IP="10.0.5.3"
+# This is the IP address of the container. You may want to set it to match
+# your own network structure such as 192.168.5.3 or similar.
 IPV4_GW="10.0.5.1/24"
+# As above, this should match the gateway of the VLAN for the container
+# network as above which is usually the .1/24 range of the IPV4_IP
+
+# if you want IPv6 support, generate a ULA, select an IP for the dns server
+# and an appropriate gateway address on the same /64 network. Make sure that
+# the 20-dns.conflist is updated appropriately. It will need the IP and GW
+# added along with a ::/0 route. Also make sure that additional --dns options
+# are passed to podman with your IPv6 DNS IPs when deploying the container for
+# the first time. You will also need to configure your VLAN to have a static
+# IPv6 block.
 
 # IPv6 Also works with Prefix Delegation from your provider. The gateway is the
 # IP of br(VLAN) and you can pick any ip address within that subnet that dhcpv6
@@ -19,21 +31,17 @@ FORCED_INTFC="br5"
 # container name; e.g. nextdns, pihole, adguardhome, etc.
 CONTAINER=nextdns
 
-## network configuration and startup:
-CNI_PATH=/mnt/data/podman/cni
-if [ ! -f "$CNI_PATH"/macvlan ]; then
-    mkdir -p $CNI_PATH
-    curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.0/cni-plugins-linux-arm64-v0.9.0.tgz | tar -xz -C $CNI_PATH
+if ! test -f /opt/cni/bin/macvlan; then
+    echo "Error: CNI plugins not found. You can install it with the following command:" >&2
+    echo "       curl -fsSLo /mnt/data/on_boot.d/05-install-cni-plugins.sh https://raw.githubusercontent.com/boostchicken/udm-utilities/master/cni-plugins/05-install-cni-plugins.sh && /bin/sh /mnt/data/on_boot.d/05-install-cni-plugins.sh" >&2
+    exit 1
 fi
 
-mkdir -p /opt/cni
-rm -f /opt/cni/bin
-ln -s $CNI_PATH /opt/cni/bin
-
+CNI_PATH=/mnt/data/podman/cni
 for file in "$CNI_PATH"/*.conflist
 do
     if [ -f "$file" ]; then
-        ln -s "$file" "/etc/cni/net.d/$(basename "$file")"
+        ln -fs "$file" "/etc/cni/net.d/$(basename "$file")"
     fi
 done
 
@@ -87,4 +95,4 @@ for intfc in ${FORCED_INTFC}; do
       fi
     done
   fi
-  done
+done
