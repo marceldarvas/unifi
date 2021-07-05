fc975584083c9ae16f8070bd44172f90a44d49fc
Marcel Darvas <marcel@overace.agency>
2021-07-05 16:58:20 -0400

    nextdns swap to pihole cloudflared

--- 10-dns nextdns.sh
+++ 10-dns nextdns.sh
@@ -29,7 +29,7 @@ IPV6_GW=""
 FORCED_INTFC="br5"
 
 # container name; e.g. nextdns, pihole, adguardhome, etc.
-CONTAINER=pihole
+CONTAINER=nextdns
 
 if ! test -f /opt/cni/bin/macvlan; then
     echo "Error: CNI plugins not found. You can install it with the following command:" >&2
