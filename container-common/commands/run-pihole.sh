adc596a928c2288ae3485e5e56ff6d97a55bb5f2
Marcel Darvas <marcel@overace.agency>
2021-07-05 16:56:25 -0400

    instructions cleanup

---
+++ container-common/commands/run-pihole.sh
@@ -0,0 +1,19 @@
+podman run -d --network dns --restart always \
+--name pihole \
+-e TZ="America/New York" \
+-v "/mnt/data/etc-pihole/:/etc/pihole/" \
+-v "/mnt/data/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+--dns=127.0.0.1 \
+--dns=1.1.1.1 \
+--hostname pi.hole \
+-e CLOUDFLARED_OPTS="--port 5053 --address 0.0.0.0" \
+-e VIRTUAL_HOST="pi.hole" \
+-e PROXY_LOCATION="pi.hole" \
+-e ServerIP="10.0.5.3" \
+-e PIHOLE_DNS_="127.0.0.1#5053" \
+-e IPv6="False" \
+boostchicken/pihole:latest
+
+
+
+podman exec -it pihole pihole -a -p PASSSWORD_HERE
