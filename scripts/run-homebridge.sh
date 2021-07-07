8650ad6bb093c8779025eb67dc043db88c548923
Marcel Darvas <hello@marceldarvas.com>
2021-07-07 10:31:46 -0400

    homebridge install

--- scripts/run-homebridge.sh
+++ scripts/run-homebridge.sh
@@ -1,13 +1,13 @@
- podman run -d --restart always \
-    --privileged \
-    --name homebridge \
-    --net homebridge \
-    --dns 10.0.5.3 \
-    --dns-search lan \
-    -e TZ=America/Detroit \
-    -e PGID=0 -e PUID=0 \
-    -e HOMEBRIDGE_CONFIG_UI=1 \
-    -e HOMEBRIDGE_CONFIG_UI_PORT=80 \
-    -v "/mnt/data/homebridge/:/homebridge/" \
-    -v "/mnt/data/homebridge/run/:/run/" \
-    oznu/homebridge:latest
+podman run -d --restart always \
+   --privileged \
+   --name homebridge \
+   --net homebridge \
+   --dns 10.0.5.3 \
+   --dns-search lan \
+   -e TZ=America/Detroit \
+   -e PGID=0 -e PUID=0 \
+   -e HOMEBRIDGE_CONFIG_UI=1 \
+   -e HOMEBRIDGE_CONFIG_UI_PORT=80 \
+   -v "/mnt/data/homebridge/:/homebridge/" \
+   -v "/mnt/data/homebridge/run/:/run/" \
+   oznu/homebridge:latest
