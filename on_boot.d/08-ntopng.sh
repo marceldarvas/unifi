73d5ae2734109b1e0bf8e2fc9f7bb365cd856fd9
Marcel Darvas <hello@marceldarvas.com>
2021-07-23 14:58:13 -0400

    refine

---
+++ on_boot.d/08-ntopng.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+CONTAINER=ntopng
+
+## network configuration and startup:
+CNI_PATH=/mnt/data/podman/cni
+if [ ! -f "$CNI_PATH"/tuning ]; then
+    mkdir -p $CNI_PATH
+    curl -L https://github.com/containernetworking/plugins/releases/download/v0.9.1/cni-plugins-linux-arm64-v0.9.1.tgz | tar -xz -C $CNI_PATH
+fi
+
+mkdir -p /opt/cni
+rm -f /opt/cni/bin
+ln -s $CNI_PATH /opt/cni/bin
+
+for file in "$CNI_PATH"/*.conflist
+do
+    if [ -f "$file" ]; then
+        ln -s "$file" "/etc/cni/net.d/$(basename "$file")"
+    fi
+done
+
+
+# Starts the ntopng container on boot.
+# All configs stored in /mnt/data/ntopng
+
+if podman container exists ${CONTAINER}; then
+  podman start ${CONTAINER}
+else
+  logger -s -t ntopng -p ERROR Container $CONTAINER not found, make sure you set the proper name, you can ignore this error if it is your first time setting it up
+fi
+
